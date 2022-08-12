325fe6b1283697fe7d633407f65c6545092cd111
victor <victor-brambilla@hotmail.com>
2022-08-12 14:54:45 -0400

    feat: create domain models

---
+++ src/domain/models/filters.ts
@@ -0,0 +1,4 @@
+export type Filter = {
+  yers: string[];
+  genres: string[];
+};
---
+++ src/domain/models/movie.ts
@@ -0,0 +1,7 @@
+export type Movie = {
+  movie_title: string;
+  release_date: string;
+  genre: string;
+  total_gross: number;
+  inflation_adjusted_gross: number;
+};
