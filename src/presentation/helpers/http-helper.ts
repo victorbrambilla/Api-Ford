efbddb1724a4eb2709ad7a93e37321a1573a2877
victor <victor-brambilla@hotmail.com>
2022-08-12 20:25:29 -0400

    feat: Create presentation helpers

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,23 @@
+import { HttpResponse } from "../protocols";
+
+export class HttpHelper {
+  static BAD_REQUEST = (err: Error): HttpResponse<Error> => ({
+    statusCode: 400,
+    body: err,
+  });
+
+  static NOT_FOUND = (err: Error): HttpResponse<Error> => ({
+    statusCode: 404,
+    body: err,
+  });
+
+  static INVALID_PARAMETERS = (err: Error): HttpResponse<Error> => ({
+    statusCode: 422,
+    body: err,
+  });
+  static OK = <T>(data: T, message?: string): HttpResponse<T> => ({
+    statusCode: 200,
+    body: data,
+    message,
+  });
+}
