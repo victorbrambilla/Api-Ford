09c4aa16c2bbaa1d586d87ac519df3212142470c
victor <victor-brambilla@hotmail.com>
2022-08-14 13:32:11 -0400

    fix: Fix methods to get movies

--- src/infra/db/repositories/movies-repository.ts
+++ src/infra/db/repositories/movies-repository.ts
@@ -4,16 +4,57 @@ import { prismaClient } from "../prismaClient";
 export class MoviesRepository implements GetMoviesRepository {
   async getMovies(params: GetMoviesRepository.params): Promise<GetMoviesRepository.result> {
     const moviesCollection = prismaClient.getConnection().movies;
-    const getMovies = await moviesCollection.findMany({
-      where: {
-        genre: {
-          in: params.filter.genres,
-        },
-        release_date: {
-          in: params.filter.years,
-        },
-      },
-    });
-    return getMovies;
+    const years = params.filter.years;
+    const genres = params.filter.genres;
+    let movies: GetMoviesRepository.result = [];
+    if (years.length === 0 && genres.length === 0) {
+      const getMovies = await moviesCollection.findMany();
+      movies = getMovies;
+    }
+    if (years.length > 0 && genres.length === 0) {
+      for (const year of years) {
+        const getMovies = await moviesCollection.findMany({
+          where: {
+            release_date: {
+              endsWith: year,
+            },
+          },
+        });
+        movies = [...movies, ...getMovies];
+      }
+    }
+
+    if (years.length === 0 && genres.length > 0) {
+      for (const genre of genres) {
+        const getMovies = await moviesCollection.findMany({
+          where: {
+            genre: {
+              contains: genre,
+            },
+          },
+        });
+        movies = [...movies, ...getMovies];
+      }
+    }
+
+    if (years.length > 0 && genres.length > 0) {
+      for (const year of years) {
+        for (const genre of genres) {
+          const getMovies = await moviesCollection.findMany({
+            where: {
+              release_date: {
+                endsWith: year,
+              },
+              genre: {
+                contains: genre,
+              },
+            },
+          });
+          movies = [...movies, ...getMovies];
+        }
+      }
+    }
+
+    return movies;
   }
 }
